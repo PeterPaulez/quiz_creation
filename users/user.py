9031d9805ff3d81209efb6d232e89f2181a4246d
Pedro Pablo Aparicio <pedroapariciocristobal@gmail.com>
2021-01-08 11:21:16 +0100

    NEW: Login action is working

--- users/user.py
+++ users/user.py
@@ -11,6 +11,18 @@ class User:
         database = sql.DataBase()
         self.password = self.encryptPass(self.password)
         database.insert('users',self)
+        return True
+
+    def login(self):
+        database = sql.DataBase()
+        self.password = self.encryptPass(self.password)
+        result = database.select('users','name,email,password','email="'+self.email+'" AND password="'+self.password+'"')
+        try:
+            if result[1] == self.email:
+                self.name = result[0]
+                return True
+        except:
+            return False
     
     def encryptPass(self,password):
         encrypt = hashlib.sha256()
